820ee2e462066797b6d2fbc0f36b80550e7adaeb
Pritam <sritam.mallick@gmail.com>
2024-05-04 21:35:00 +0530

    Added all CRUD operations for Job Applications and Status

--- backend/runtime_services/controllers/job_applications_controller.py
+++ backend/runtime_services/controllers/job_applications_controller.py
@@ -1,31 +1,125 @@
 import json
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
-from webapi.models import JobApplication
+from webapi.models import JobApplication, Company, ApplicationStatus
 from django.core import serializers
+from controllers.serializers.job_applications_serializer import JobApplicationSerializer
+from rest_framework import status
+from json.decoder import JSONDecodeError
+from django.db.utils import IntegrityError
+import datetime
 
 def getByCompanyId(request, companyId):
     objects = JobApplication.objects.filter(company=companyId)
     if len(objects) == 0:
-        return HttpResponseNotFound("Not Found")
+        return HttpResponseNotFound("JobApplication Not Found")
     
     data = serializers.serialize('json', objects)
     return HttpResponse(data, 'application/json')
+
 
 def getByCompanyIdAndApplicationId(request, companyId, applicationId):
     objects = JobApplication.objects.filter(id=applicationId, company=companyId)
     if len(objects) == 0:
-        return HttpResponseNotFound("Not Found")
+        return HttpResponseNotFound("JobApplication Not Found")
     
     data = serializers.serialize('json', objects)
     return HttpResponse(data, 'application/json')
+
 
 def getAll(request):
     objects = JobApplication.objects.all()
-    if len(objects) == 0:
-        return HttpResponseNotFound("Not Found")
-    
     data = serializers.serialize('json', objects)
     return HttpResponse(data, 'application/json')
 
-def post(request):
-    return JsonResponse('hi')
+
+def postByCompanyId(request, companyId):
+    company = Company.objects.filter(pk=companyId)
+    if len(company) == 0:
+        return HttpResponseNotFound("Company Not Found")
+    
+    try:
+        model = json.loads(request.body.decode('utf-8'))
+        status = ApplicationStatus.objects.filter(pk=model['status'])
+        if len(status) == 0:
+            return HttpResponseNotFound("ApplicationStatus Not Found")
+        
+        entity = JobApplicationSerializer(data=model)
+        print(entity)
+        if entity.is_valid():
+            entity.validated_data['createdAt'] = datetime.datetime.now()
+            entity.validated_data['updatedAt'] = datetime.datetime.now()
+            entity.validated_data['company'] = company[0]
+            entity.save()
+            data = serializers.serialize('json', [entity.instance])
+            return HttpResponse(data, 'application/json')
+        else:
+            return BadRequestHandler("Request payload failed schema validation")
+    except JSONDecodeError as e:
+        return BadRequestHandler(e.args[0])
+    except IntegrityError as e:
+        return BadRequestHandler(e.args[1])
+    
+
+def patchByCompanyIdAndApplicationId(request, companyId, jobApplicationId):
+    company = Company.objects.filter(pk=companyId)
+    if len(company) == 0:
+        return HttpResponseNotFound("Company Not Found")
+    
+    objects = JobApplication.objects.filter(pk=jobApplicationId)
+    if len(objects) == 0:
+        return HttpResponseNotFound("JobApplication Not Found")
+    
+    try:
+        model = json.loads(request.body.decode('utf-8'))
+        status = ApplicationStatus.objects.filter(pk=model['status'])
+        if len(status) == 0:
+            return HttpResponseNotFound("ApplicationStatus Not Found")
+        
+        entity = JobApplicationSerializer(data=model)
+        savedEntity = objects[0]
+        print(entity)
+        if entity.is_valid():
+            savedEntity.updatedAt = datetime.datetime.now()
+            savedEntity.role = entity.validated_data['role']
+            savedEntity.ctc = entity.validated_data['ctc']
+            savedEntity.currency = entity.validated_data['currency']
+            savedEntity.officeLocation = entity.validated_data['officeLocation']
+            savedEntity.status = entity.validated_data['status']
+            savedEntity.save()
+            data = serializers.serialize('json', [savedEntity])
+            return HttpResponse(data, 'application/json')
+        else:
+            return BadRequestHandler("Request payload failed schema validation")
+    except JSONDecodeError as e:
+        return BadRequestHandler(e.args[0])
+    except IntegrityError as e:
+        return BadRequestHandler(e.args[1])
+    
+
+def deleteByCompanyIdAndApplicationId(request, companyId, jobApplicationId):
+    company = Company.objects.filter(pk=companyId)
+    if len(company) == 0:
+        return HttpResponseNotFound("Company Not Found")
+    
+    objects = JobApplication.objects.filter(pk=jobApplicationId)
+    if len(objects) == 0:
+        return HttpResponseNotFound("JobApplication Not Found")
+    
+    entity = objects[0]
+    entity.delete()
+    data = serializers.serialize('json', objects)
+    return HttpResponse(data, 'application/json')
+    
+
+def BadRequestHandler(e):
+    error = {
+        "message": e,
+        "schema": {
+            "role" : "",
+            "ctc": 0,
+            "currency": "",
+            "officeLocation": "",
+            "status": 0
+        }
+    }
+    return JsonResponse(error, status=status.HTTP_400_BAD_REQUEST)
--- backend/runtime_services/webapi/urls.py
+++ backend/runtime_services/webapi/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import main, pdf_to_image, companies, company, jobApplicationsByCompanyAndApplication, jobApplicationsByCompany, jobApplications
+from .views import main, pdf_to_image, companies, company, jobApplicationsByCompanyAndApplication, jobApplicationsByCompany, jobApplications, appliationStatuses
 
 urlpatterns = [
     path("", main),
     path("pdf-to-image/", pdf_to_image),
     path("companies/<int:companyId>/", company),
     path("companies/", companies),
-    path("companies/<int:companyId>/jobs-applications/<int:jobApplicationId>/", jobApplicationsByCompanyAndApplication),
-    path("companies/<int:companyId>/jobs-applications/", jobApplicationsByCompany),
-    path("jobs-applications/", jobApplications)
+    path("companies/<int:companyId>/job-applications/<int:jobApplicationId>/", jobApplicationsByCompanyAndApplication),
+    path("companies/<int:companyId>/job-applications/", jobApplicationsByCompany),
+    path("job-applications/", jobApplications),
+    path("application-status/", appliationStatuses)
 ]
